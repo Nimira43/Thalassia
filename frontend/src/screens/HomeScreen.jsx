39b7e2bb94d4f6a924e383da48e73b8d41e19e6a
Nimira43 <n.rabone72@aol.com>
2025-11-27 20:07:23 +0000

    Importing HomeScreen into App

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -7,8 +7,9 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
       <Row>
         {products.map(
-          () => (
+          (product) => (
             <Col sm={12} md={6} lg={4} xl={3}>
+              <h3>{product.name}</h3>
             </Col>
           )
         )}
