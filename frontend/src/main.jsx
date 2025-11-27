02ed76966abdb22eb9ce6981c5f3d2e5c72c1885
Nimira43 <n.rabone72@aol.com>
2025-11-27 22:49:41 +0000

    Creating router - App and HomePage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,6 +10,22 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './index.css'
 import App from './App.jsx'
+import HomePage from './pages/HomePage.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path='/'
+      element={<App />}
+    >
+      <Route
+        index={true}
+        path='/'
+        element={<HomePage />}
+      />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
