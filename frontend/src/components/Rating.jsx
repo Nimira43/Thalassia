ee7d8e3e7d7066a9964c59fd85d92c7762efe3b2
Nimira43 <n.rabone72@aol.com>
2025-11-27 23:49:20 +0000

    Alternative version

--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -1,53 +1,76 @@
+// import { PiStarFill, PiStarHalfFill, PiStar } from 'react-icons/pi'
+
+// const Rating = ({ value, text }) => {
+//   return ( 
+//     <div className='rating'>
+//       <span>
+//         {value >= 1
+//           ? <PiStarFill />
+//           : value >= 0.5
+//             ? <PiStarHalfFill />
+//             : <PiStar />
+//         }
+//       </span>
+//       <span>
+//         {value >= 2
+//           ? <PiStarFill />
+//           : value >= 1.5
+//             ? <PiStarHalfFill />
+//             : <PiStar />
+//         }
+//       </span>
+//       <span>
+//         {value >= 3
+//           ? <PiStarFill />
+//           : value >= 2.5
+//             ? <PiStarHalfFill />
+//             : <PiStar />
+//         }
+//       </span>
+//       <span>
+//         {value >= 4
+//           ? <PiStarFill />
+//           : value >= 3.5
+//             ? <PiStarHalfFill />
+//             : <PiStar />
+//         }
+//       </span>
+//       <span>
+//         {value >= 5
+//           ? <PiStarFill />
+//           : value >= 4.5
+//             ? <PiStarHalfFill />
+//             : <PiStar />
+//         }
+//       </span>
+//       <span className='rating-text'>
+//         { text && text }
+//       </span>
+//     </div>
+//    )
+// }
+ 
+// export default Rating
+
 import { PiStarFill, PiStarHalfFill, PiStar } from 'react-icons/pi'
 
 const Rating = ({ value, text }) => {
-  return ( 
+  const stars = [1, 2, 3, 4, 5]
+
+  const renderStar = (starValue) => {
+    if (value >= starValue) return <PiStarFill />
+    if (value >= starValue - 0.5) return <PiStarHalfFill />
+    return <PiStar />
+  }
+
+  return (
     <div className='rating'>
-      <span>
-        {value >= 1
-          ? <PiStarFill />
-          : value >= 0.5
-            ? <PiStarHalfFill />
-            : <PiStar />
-        }
-      </span>
-      <span>
-        {value >= 2
-          ? <PiStarFill />
-          : value >= 1.5
-            ? <PiStarHalfFill />
-            : <PiStar />
-        }
-      </span>
-      <span>
-        {value >= 3
-          ? <PiStarFill />
-          : value >= 2.5
-            ? <PiStarHalfFill />
-            : <PiStar />
-        }
-      </span>
-      <span>
-        {value >= 4
-          ? <PiStarFill />
-          : value >= 3.5
-            ? <PiStarHalfFill />
-            : <PiStar />
-        }
-      </span>
-      <span>
-        {value >= 5
-          ? <PiStarFill />
-          : value >= 4.5
-            ? <PiStarHalfFill />
-            : <PiStar />
-        }
-      </span>
-      <span className='rating-text'>
-        { text && text }
-      </span>
+      {stars.map((star) => (
+        <span key={star}>{renderStar(star)}</span>
+      ))}
+      <span className="rating-text">{text && text}</span>
     </div>
-   )
+  )
 }
- 
+
 export default Rating
