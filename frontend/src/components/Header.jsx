a0f4b4d43c54eebe0d1f76ebf645cf0700d3696f
Nimira43 <n.rabone72@aol.com>
2025-11-27 17:36:17 +0000

    Updated custom colour CSS

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -17,8 +17,20 @@ const Header = () => {
             Thalassia
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse ibasic-navbar-nav>
+            <Nav className='ms-auto'>
+              <Nav.Link
+                href='/cart'
+              >
+                <PiShoppingCart /> Cart
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
+      <div>
+        <h1 className='text-main hover-transition'>Test</h1>
+      </div>
     </header>
   )
 }
