716042f1a281ccbf75feda9481f1521b714e1d0e
Nimira43 <n.rabone72@aol.com>
2025-11-28 09:15:53 +0000

    Setting up server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,10 @@
 import express from 'express'
 
-const PORT = 5000
+const port = 5000
 const app = express()
+
+app.get('/', (req, res) => {
+  res.send('API is running')
+})
+
+app.listen(port, () => console.log(`Server running on Port ${port}`))
