8dd9d1cdfd040972a0bc01c5f7450f2ab5f31d7b
Nimira43 <n.rabone72@aol.com>
2025-11-28 00:45:40 +0000

    ProductsPage - 3

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -6,7 +6,7 @@ import products from '../products'
 
 const ProductPage = () => {
   const { id: productId } = useParams()
-  const product = [products.find((p) => p._id === productId)]
+  const product = products.find((p) => p._id === productId)
   
   return ( 
     <>
@@ -18,8 +18,24 @@ const ProductPage = () => {
       </Link>
       <Row>
         <Col md={5}>
+          <Image
+            src={product.image}
+            alt={product.name}
+            fluid
+          />
         </Col>
         <Col md={4}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+          </ListGroup>
         </Col>
         <Col md={3}>
         </Col>
