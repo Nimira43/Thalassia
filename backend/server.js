6af69c4f60272902292d2d8a4b3a8bb86a8d16ea
Nimira43 <n.rabone72@aol.com>
2025-11-28 09:51:42 +0000

    Updating new port settings

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 import products from './data/products.js'
 
-const port = 5000
+const port = process.env.PORT || 5000
 const app = express()
 
 app.get('/', (req, res) => {
