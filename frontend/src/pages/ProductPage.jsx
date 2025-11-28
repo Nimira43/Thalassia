c05f811c842d607b38b53683a743f00f9d737df8
Nimira43 <n.rabone72@aol.com>
2025-11-28 00:33:32 +0000

    Brought in imports for ProductPage

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,4 +1,6 @@
 import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import products from '../products'
 
 const ProductPage = () => {
@@ -6,7 +8,8 @@ const ProductPage = () => {
   const product = [products.find((p) => p._id === productId)]
   
   return ( 
-    <div>ProductPage</div>
+    <>
+    </>
   )
 }
  
