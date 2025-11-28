661277cb20294891483dd8c8869d2dedd5ebaf32
Nimira43 <n.rabone72@aol.com>
2025-11-28 18:50:21 +0000

    Succesfully connecting the Mongo DB container to the backend server.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js' 
 dotenv.config()
 import products from './data/products.js'
+
+connectDB()
 
 const port = process.env.PORT || 5000
 const app = express()
