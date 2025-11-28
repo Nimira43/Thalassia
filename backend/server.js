15247e126e024058f3e8f81d8d58578e827ea998
Nimira43 <n.rabone72@aol.com>
2025-11-28 09:20:08 +0000

    Made a copy of products.js and placed inside new data folder in backend.

--- backend/server.js
+++ backend/server.js
@@ -7,4 +7,4 @@ app.get('/', (req, res) => {
   res.send('API is running')
 })
 
-app.listen(port, () => console.log(`Server running on Port ${port}`))
+app.listen(port, () => console.log(`Server running on Port ${port}`)) 
