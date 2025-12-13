6ede6894bb0f1759e8a1024391f852aa95f5054b
Nimira43 <n.rabone72@aol.com>
2025-12-13 23:33:15 +0000

    Two more test users made.

--- backend/data/users.js
+++ backend/data/users.js
@@ -6,5 +6,17 @@ const users = [
     email: 'admin@email.com',
     pasword: bcrypt.hashSync('123456', 10),
     isAdmin: true,
-  }
+  },
+  {
+    name: 'John Doe',
+    email: 'john@email.com',
+    pasword: bcrypt.hashSync('123456', 10),
+    isAdmin: false,
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@email.com',
+    pasword: bcrypt.hashSync('123456', 10),
+    isAdmin: false,
+  },
 ]
