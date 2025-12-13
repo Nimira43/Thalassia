425de752d6a8932df574c42543433b50ba36c7ae
Nimira43 <n.rabone72@aol.com>
2025-12-13 21:13:27 +0000

    productSchema - 2

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -4,5 +4,13 @@ const productSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
   }
 })
