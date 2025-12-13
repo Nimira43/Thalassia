595620a0283a2a92e124a3080baaccc494522fa2
Nimira43 <n.rabone72@aol.com>
2025-12-13 21:18:34 +0000

    productSchema - 6

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -26,5 +26,11 @@ const productSchema = new mongoose.Schema({
     type: Number,
     required: true,
     default: 0,
+  },
+  numReviews: {
+    type: Number,
+    required: true,
+    default: 0,
+  
   }
 })
