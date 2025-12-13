82e77199f7245e31099759ff0f33c9335dacd894
Nimira43 <n.rabone72@aol.com>
2025-12-13 23:40:53 +0000

    Imports for seeder.js

---
+++ backend/seeder.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+import dotnet from 'dotnet'
+import colors from 'colors'
+import users from './data/users.js'
+import products from './data/products.js'
+import User from './models/userModel.js'
+import Product from './models/productModel.js'
+import Order from './models/orderModel.js'
+import connectDB from './config/db.js'
+
