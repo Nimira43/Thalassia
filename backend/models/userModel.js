95ddbbc709fc402f4d92e13cac2b84bd5b5d46f5
Nimira43 <n.rabone72@aol.com>
2025-12-13 22:27:05 +0000

    userSchema - 3

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,6 +3,15 @@ import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
   },
 })
