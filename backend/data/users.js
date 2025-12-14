1e9ba64d0853e790e107c0e9e7baeb550a82c4e5
Nimira43 <n.rabone72@aol.com>
2025-12-14 00:25:31 +0000

    Corrected typos in users.js and seeder.js. Wrote scripts to import and destroy the data. Ran import and Mongo DB container is seeded.

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,19 +4,19 @@ const users = [
   {
     name: 'Admin User',
     email: 'admin@email.com',
-    pasword: bcrypt.hashSync('123456', 10),
+    password: bcrypt.hashSync('123456', 10),
     isAdmin: true,
   },
   {
     name: 'John Doe',
     email: 'john@email.com',
-    pasword: bcrypt.hashSync('123456', 10),
+    password: bcrypt.hashSync('123456', 10),
     isAdmin: false,
   },
   {
     name: 'Jane Doe',
     email: 'jane@email.com',
-    pasword: bcrypt.hashSync('123456', 10),
+    password: bcrypt.hashSync('123456', 10),
     isAdmin: false,
   },
 ]
--- backend/seeder.js
+++ backend/seeder.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-import dotnet from 'dotnet'
+import dotenv from 'dotenv'
 import colors from 'colors'
 import users from './data/users.js'
 import products from './data/products.js'
