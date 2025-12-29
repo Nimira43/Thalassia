cf916649040c36c5c8fd5a88c1379113636090fb
Nimira43 <n.rabone72@aol.com>
2025-12-29 23:37:00 +0000

    Moved product import into productRoutes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js' 
 dotenv.config()
-import products from './data/products.js'
 import productRoutes from './routes/productRoutes.js'
 
 connectDB()
