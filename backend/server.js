fd8963206a65c04f5f5cad5e609cc053e57fa09e
Nimira43 <n.rabone72@aol.com>
2025-12-29 23:33:57 +0000

    Importing productRoutes into server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js' 
 dotenv.config()
 import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
 
 connectDB()
 
