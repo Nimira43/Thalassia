5c241854ce349008f9842d9882e8855cccc7ffc0
Nimira43 <n.rabone72@aol.com>
2025-12-29 23:50:40 +0000

    asyncHandler update - 1

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,15 @@
 import express from 'express'
 const router = express.Router()
 import asyncHandler from '../middleware/asyncHandler'
+import Product from '../models/productModel'
 
-router.get('/', (req, res) => {
+router.get('/', asyncHandler(async (req, res) => {
   res.json(products)
-})
+}))
 
-router.get('/:id', (req, res) => {
+router.get('/:id', asyncHandler(async (req, res) => {
   const product = products.find((p) => p._id === req.params.id)
   res.json(product)
-})
+}))
 
 export default router
