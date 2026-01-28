5cc2692e431c5884df43196bb478170b3026c965
Nimira43 <n.rabone72@aol.com>
2026-01-28 14:33:27 +0000

    Redux store and API slice

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,8 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './index.css'
@@ -33,6 +35,8 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
