6df270a6e2748c3b6be42fb2167ecdd2e0f2d78b
Nimira43 <n.rabone72@aol.com>
2026-02-13 14:06:58 +0000

    Using Redux in ProductPage

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,3 @@
-// export const BASE_URL =
-//   import.meta.env.NODE_ENV === 'development'
-//     ? 'http://localhost:5000'
-//     : ''
-
 export const BASE_URL = ''
 export const PRODUCTS_URL = '/api/products'
 export const USERS_URL = '/api/users'
