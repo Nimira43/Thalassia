c0e6a6f6d9afcba3a4a089abb1385e9a8e540245
Nimira43 <n.rabone72@aol.com>
2026-04-03 19:02:57 +0100

    Auth middleware.

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken'
-import asyncHandler from './asyncHandler'
-import User from '../models/userModel'
+import asyncHandler from './asyncHandler.js'
+import User from '../models/userModel.js'
 
-export const protect = asyncHandler(async (req, res, next) => {
+const protect = asyncHandler(async (req, res, next) => {
   let token 
 
   token = req.cookies.jwt
@@ -21,3 +21,17 @@ export const protect = asyncHandler(async (req, res, next) => {
     throw new Error('Not authorised - no token.')
   }
 })
+
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next()
+  } else {
+    res.status(401)
+    throw new Error('Not authorised as admin.')
+  }
+}
+
+export {
+  protect,
+  admin
+}
