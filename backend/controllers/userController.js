42db15965eac28f2abdc0a85d476354a878551ea
Nimira43 <n.rabone72@aol.com>
2026-04-14 20:14:08 +0100

    getUserProfile function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,7 @@ const authUser = asyncHandler(async (req, res) => {
   if (user && (await user.matchPassword(password))) {
     generateToken(res, user._id)
     
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -28,7 +28,7 @@ const registerUser = asyncHandler(async (req, res) => {
   const userExists = await User.findOne({ email })
 
   if (userExists) {
-    res.status(400)
+    res.status(400) 
     throw new Error('User already exists.')
   }
 
@@ -65,7 +65,19 @@ const logoutUser = asyncHandler(async (req, res) => {
 })
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('Get User Profile')
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404) 
+    throw new Error('User not found.')
+  }
 })
 
 const updateUserProfile = asyncHandler(async (req, res) => {
