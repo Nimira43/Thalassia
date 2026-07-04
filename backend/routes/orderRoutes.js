858dc6918ef906ddf7b77a298a2b510623c4bcbf
Nimira43 <n.rabone72@aol.com>
2026-07-04 10:00:30 +0100

    Updated PlaceOrderPage

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -19,7 +19,8 @@ router
   .get(protect, getMyOrders)
 router
   .route('/:id')
-  .get(protect, admin, getOrderById)
+  .get(protect, getOrderById)
+  // .get(protect, admin, getOrderById)
 router
   .route('/:id/pay')
   .put(protect, updateOrderToPaid)
