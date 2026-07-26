bdfb626eb65426bf5eb9bcb3646f9eda6fe0a44d
Nimira43 <n.rabone72@aol.com>
2026-07-26 04:56:45 +0100

    Installed PayPal/React-PayPal. Setup PayPalScriptProvider.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom'
+import {PayPalScriptProvider} from '@paypal/react-paypal-js'
 import { Provider } from 'react-redux'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -77,7 +78,9 @@ const router = createBrowserRouter(
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
   </StrictMode>,
 )
